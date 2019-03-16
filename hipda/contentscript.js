f1c424f3443b3af986c189b13b85053c7a0bdd1b
fqxufo <fqxufo@gmail.com>
2019-03-16 23:43:24 +0800

    完成黑名单用户屏蔽显示功能
    完善了全文搜索功能,可以在全文搜索结果页面自动选中全文选项

--- hipda/contentscript.js
+++ hipda/contentscript.js
@@ -1,33 +1,58 @@
-$(function(){
-    $('#wrap > form > label').text('fuck yeah');
+//恢复全文搜索
+function reviveFullSearch(url) {
+    //在搜索页面添加全文搜索的选项
+    if (url.indexOf('search') > 0) {
+        $('#wrap > form > p.searchkey > select').append($('<option>', {
+            value: 'fulltext',
+            text: '全文'
+        }));
+    }
 
-    $('#wrap > form > p.searchkey > select').append($('<option>', {
-        value: 'fulltext',
-        text: '全文'
-    }));
+    //全文搜索结果页自动选中全文搜索选项
+    if (url.indexOf('srchtype=fulltext') > 0) {
+        $('#wrap > form > p.searchkey > select').val('fulltext');
+    }
 
-    url = GBK.URI.encodeURI('https://www.hi-pda.com/forum/pm.php?action=delblack&formhash=08f55da1&user=德味儿');
-    console.log(url);
-    // $.get(url);
-    url2 = 'https://www.hi-pda.com/forum/pm.php?action=addblack&formhash=08f55da1&user=uglee';
-    console.log(url2);
-    // $.get(url2);
+}
+
+
+//黑名单屏蔽功能
+function block(url, blacklist) {
+    //帖子列表页面屏蔽
+    if (url.indexOf('fid=') > 0) {
+        var authorList = $('.author>cite>a')
+        $(authorList).each(function () {
+            var userName = $(this).text();
+            if (blacklist.indexOf(userName) > 0) {
+                $(this).parents('tbody').hide();
+            }
+        });
+    }
+
+    //帖子内容页面屏蔽
+    if (url.indexOf('viewthread')>0){
+        var postList = $('.mainbox>div')
+        $(postList).each(function(){
+            var userName = $('.postinfo>a',this).text()
+            if (blacklist.indexOf(userName)>0){
+                $(this).hide();
+            }
+        })
+    }
+}
+
+
+
+//脚本主入口,页面加载时执行
+$(function () {
+    var urlOfPage = window.location.href;
+    reviveFullSearch(urlOfPage);
+
+    //通过chrome.storage获取黑名单,进行屏蔽功能
+    chrome.storage.local.get('blacklist', function (result) {
+        console.log('Value currently is ' + result.blacklist);
+        block(urlOfPage, result.blacklist);
+    });
 })
 
-$.get( "https://www.hi-pda.com/forum/pm.php?action=viewblack", function( data ) {
 
-var arr = [];
- 
-var el = $( '<div></div>' );
-el.html(data);
-$('.blacklist  a[class=remove]',el).each(function(){
-    var gbkusername = $(this).attr('href').split("user=")[1];
-    decodedusername = GBK.URI.decodeURI(gbkusername);
-    arr.push(decodedusername);
-
-
-
-});
-console.log(arr);
-
-});
