2afda85f53e4f8e8505de1688841a1469ba45adb
fqxufo <fqxufo@gmail.com>
2019-03-21 23:36:20 +0800

    增加功能选择页面,完成配置文件存储逻辑

--- hipda/popup/popup.js
+++ hipda/popup/popup.js
@@ -1,7 +1,127 @@
-window.onload = function () {
-    var manifestData = chrome.runtime.getManifest();
-    console.log(manifestData.version);
+// 默认选项
+var defaultConfig = {
+    enableBlacklist: true,
+    pageWidth: "100%",
+    goodboySee: true,
+    blockBSTop: true,
+    highlightOP: true
+}
 
-    document.getElementById('vernumber').innerText = 'v' + manifestData.version;
+
+
+//初始化所有ui
+function showOption(currentConfig) {
+    $('[id^=toggle_]').bootstrapSwitch('state', false);
+
+
+    var manifestData = chrome.runtime.getManifest();
+
+    $('#vernumber').text('v' + manifestData.version);
+    $('.bootstrap-switch-wrapper').addClass('float-right');
+
+
+
+
+
+    //下面开始根据配置文件改变UI显示
+
+    if (currentConfig.enableBlacklist) {
+        $('#toggle_blacklist').bootstrapSwitch('state', true);
+    }
+    if (currentConfig.goodboySee) {
+        $('#toggle_goodboy').bootstrapSwitch('state', true);
+    }
+    if (currentConfig.blockBSTop) {
+        $('#toggle_BSTop').bootstrapSwitch('state', true);
+    }
+    if (currentConfig.highlightOP) {
+        $('#toggle_highlightop').bootstrapSwitch('state', true);
+    }
+
+    $('#btnpageWidth').text(currentConfig.pageWidth);
+
+
+
 
 }
+
+
+//根据用户操作改变UI
+function userChangeOption(currentConfig) {
+    $('#toggle_blacklist').on('switchChange.bootstrapSwitch', function (event, state) {
+        currentConfig.enableBlacklist = state;
+        console.log(currentConfig);
+    });
+    $('#toggle_goodboy').on('switchChange.bootstrapSwitch', function (event, state) {
+        currentConfig.goodboySee = state;
+        console.log(currentConfig);
+    });
+    $('#toggle_BSTop').on('switchChange.bootstrapSwitch', function (event, state) {
+        currentConfig.blockBSTop = state;
+        console.log(currentConfig);
+    });
+    $('#toggle_highlightop').on('switchChange.bootstrapSwitch', function (event, state) {
+        currentConfig.highlightOP = state;
+        console.log(currentConfig);
+    });
+
+    $('.droppagewidth>a').click(function () {
+        $('#btnpageWidth').text($(this).text());
+        currentConfig.pageWidth = $(this).text()
+        console.log(currentConfig);
+    })
+}
+
+//确认配置,存储到chrome.storage
+function confirmChange(currentConfig) {
+    $('#confirm').click(function () {
+        chrome.storage.sync.set({ 'extentionConfig': currentConfig });
+        $('.alert-success').fadeIn(1000);
+
+        $('.alert-success').delay(2000).fadeOut(1000);
+    });
+}
+
+//恢复默认配置
+function backToDefault(defaultConfig) {
+    $('#backToDefault').click(function () {
+        showOption(defaultConfig);
+        chrome.storage.sync.set({ 'extentionConfig': defaultConfig });
+
+    });
+}
+
+
+$(function () {
+
+    var currentConfig = JSON.parse(JSON.stringify(defaultConfig));
+
+    chrome.storage.sync.get('extentionConfig', function (obj) {
+
+        if (typeof obj.extentionConfig == 'undefined') {
+            chrome.storage.sync.set({ 'extentionConfig': defaultConfig });
+        }
+
+        if (typeof obj.extentionConfig !== 'undefined') {
+            currentConfig = obj.extentionConfig;
+        }
+        showOption(currentConfig);
+        userChangeOption(currentConfig);
+        confirmChange(currentConfig);
+        backToDefault(defaultConfig);
+        console.log(currentConfig);
+
+    });
+
+
+
+
+
+
+
+
+
+}
+);
+
+
